99f800369e9a99aec6a482ed66255a5f6cb4f721
shaswatkr <shaswatkr96@gmail.com>
2019-12-11 02:45:17 +0530

    Component created for Shopping List

---
+++ src/components/counter.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { value } = this.props.counter;
+    return value === 0 ? "Zero" : this.props.counter.value;
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.children}
+        <span className={this.getBadgeClass()}> {this.formatCount()} </span>
+        <button
+          className="btn btn-secondary btn-sm"
+          onClick={() => this.props.onIncrement(this.props.counter)}
+        >
+          Increment
+        </button>
+        <button
+          className="btn btn-warning btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Counter;
---
+++ src/components/counters.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    return (
+      <div>
+        <button
+          className="btn btn-primary btn-md m-2"
+          onClick={() => this.props.onReset()}
+        >
+          Reset
+        </button>
+        {this.props.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            counter={counter}
+            onDelete={this.props.onDelete}
+            onIncrement={this.props.onIncrement}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
